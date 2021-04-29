cb87402fb6d55c00eba2bbe84280832b746a71a2
Runal <runalb@users.noreply.github.com>
2021-04-29 13:52:48 +0530

    added: product name

--- Amazon_Price_Tracker.py
+++ Amazon_Price_Tracker.py
@@ -3,12 +3,24 @@ import requests
 import smtplib
 import time
 
-def get_product_price(url):
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    req = requests.get(url, headers = headers)
+def get_product_page(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+    req = requests.get(url, headers=headers)
+    global soup
     soup = BeautifulSoup(req.content, 'html.parser')
 
+
+
+def get_product_name():
+    product_name_tag = soup.find("span", class_="product-title-word-break")
+    name = product_name_tag.text.strip()
+    return name
+
+
+
+def get_product_price():
     price_tag = soup.find('span', id ='priceblock_ourprice')
     if price_tag is None:
         price_tag = soup.find('span', id ='priceblock_dealprice')
@@ -53,7 +65,7 @@ def send_mail():
 def check_product_price_with_your_price(product_p,your_p):
     if product_p < your_p:
         print("Less")
-        send_mail()
+        #send_mail()
     elif product_p <= your_p:
         print("same")
     else:
@@ -67,13 +79,24 @@ def check_product_price_with_your_price(product_p,your_p):
 your_price = 14499
 url = "https://www.amazon.in/Amazfit-AMOLED-Display-Monitor-Bluetooth-Storage/dp/B08XW3TKYR/ref=sr_1_2"
 
+'''
 sender_email =
 sender_email_password =
 receiver_email =
+'''
+
 
 while(True):
-    product_price = get_product_price(url)
+    get_product_page(url)
+
+    product_name = get_product_name()
+    #print(product_name)
+
+    product_price = get_product_price()
+
     check_product_price_with_your_price(product_price,your_price)
+
+
     time.sleep(50000)
 
 
