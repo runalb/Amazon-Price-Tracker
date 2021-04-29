9e79e34f8041ce062264c0ee76cf7097c5572d99
Runal <runalb@users.noreply.github.com>
2021-04-29 12:32:10 +0530

    get price from amazon

--- Amazon_Price_Tracker.py
+++ Amazon_Price_Tracker.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
+#import lxml
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
